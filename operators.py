caacb0a10c54a29396236b6d300ab0faa861d518
arai05 <izteleuovaarailym78@gmail.com>
2023-01-24 16:04:26 +0600

    all

---
+++ operators.py
@@ -0,0 +1,134 @@
+print(10 + 5)
+
+x = 5
+y = 3
+
+print(x + y)
+print(x - y)
+print(x*y)
+
+x = 12
+y = 3
+
+print(x / y)
+
+
+x = 5
+y = 2
+
+print(x % y)
+
+x = 2
+y = 5
+
+print(x ** y)
+
+x = 15
+y = 2
+
+print(x // y)
+
+x = 5
+print(x)
+
+x = 5
+x += 3
+print(x)
+
+x = 5
+x -= 3
+print(x)
+
+x = 5
+x *= 3
+print(x)
+
+x = 12
+x /= 3
+print(x)
+
+x = 14
+x %= 3
+print(x)
+
+x = 14
+x //= 3
+print(x)
+
+x = 2
+x **=2
+print(x)
+
+x = 5
+x &= 3
+print(x)
+
+x = 5
+x |= 3
+print(x)
+
+x = 5
+x ^= 3
+print(x)
+
+x = 5
+x >>= 3
+print(x)
+
+x = 5
+x <<= 3
+print(x)
+
+x = 5
+y = 3
+print(x == y)
+
+x = 5
+y = 3
+print(x != y)
+
+x = 5
+y = 3
+print(x > y)
+
+x = 5
+y = 3
+print(x >= y)
+
+x = 5
+y = 3
+print(x <= y)
+
+x = 5
+print(x > 3 and x < 10)
+
+x= 5
+print(x > 3 or x < 4)
+
+x = 5
+print(not(x > 3 and x < 10))
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+x = ["apple", "banana"]
+
+print("pineapple" not in x)
+
+
+
+
+
+
+
+
+

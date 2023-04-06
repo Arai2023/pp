d79ac4e9b298867fa0b36c26f0a63dfde4172894
Arai2023 <122877516+Arai2023@users.noreply.github.com>
2023-04-06 21:19:58 +0600

    Add files via upload

---
+++ WEEK11/pygame3.py
@@ -0,0 +1,109 @@
+import pygame, math
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    clock = pygame.time.Clock()
+    
+    radius = 15
+    x = 0
+    y = 0
+    mode = 'blue'
+    points = []
+    
+    while True:
+        
+        pressed = pygame.key.get_pressed()
+        
+        alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
+        ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
+        
+        for event in pygame.event.get():
+            
+            # determin if X was clicked, or Ctrl+W or Alt+F4 was used
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w and ctrl_held:
+                    return
+                if event.key == pygame.K_F4 and alt_held:
+                    return
+                if event.key == pygame.K_ESCAPE:
+                    return
+            
+                # determine if a letter key was pressed
+                if event.key == pygame.K_r:
+                    mode = 'red'
+                elif event.key == pygame.K_g:
+                    mode = 'green'
+                elif event.key == pygame.K_b:
+                    mode = 'blue'
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: # left click grows radius
+                    radius = min(200, radius + 1)
+                elif event.button == 3: # right click shrinks radius
+                    radius = max(1, radius - 1)
+            
+            if event.type == pygame.MOUSEMOTION:
+                # if mouse moved, add point to list
+                position = event.pos
+                points = points + [position]
+                points = points[-256:]
+                
+        screen.fill((0, 0, 0))
+        
+        # draw all points
+        i = 0
+        while i < len(points) - 1:
+            drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
+            i += 1
+        
+        pygame.display.flip()
+        
+        clock.tick(60)
+
+def drawLineBetween(screen, index, start, end, width, color_mode):
+    c1 = max(0, min(255, 2 * index - 256))
+    c2 = max(0, min(255, 2 * index))
+    
+    if color_mode == 'blue':
+        color = (c1, c1, c2)
+    elif color_mode == 'red':
+        color = (c2, c1, c1)
+    elif color_mode == 'green':
+        color = (c1, c2, c1)
+    
+    dx = start[0] - end[0]
+    dy = start[1] - end[1]
+    iterations = max(abs(dx), abs(dy))
+    
+    for i in range(iterations):
+        progress = 1.0 * i / iterations
+        aprogress = 1 - progress
+        x = int(aprogress * start[0] + progress * end[0])
+        y = int(aprogress * start[1] + progress * end[1])
+        pygame.draw.circle(screen, color, (x, y), width)
+
+
+
+def draw(self, screen, start, end):
+    if self.tool_type == 'pen':
+        pygame.draw.line(screen, self.color, start, end, self.size)
+    elif self.tool_type == 'rectangle':
+        pygame.draw.rect(screen, self.color, (start[0], start[1], end[0]-start[0], end[1]-start[1]), self.size)
+    elif self.tool_type == 'circle':
+        radius = int(math.sqrt((end[0]-start[0])**2 + (end[1]-start[1])**2))
+        pygame.draw.circle(screen, self.color, start, radius, self.size)
+    elif self.tool_type == 'eraser':
+        pygame.draw.line(screen, self.color, start, end, self.size)
+
+# create new display for color selection
+color_palette = pygame.display.set_mode((200, 150))
+colors = [(255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
+for i, color in enumerate(colors):
+    pygame.draw.rect(color_palette, color, (i*25, 0, 25, 25))
+pygame.display.set_caption("Select Color")
+
+
+main()
